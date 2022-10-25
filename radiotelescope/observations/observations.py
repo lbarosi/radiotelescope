c3cf776b0ff928c210559f59080b9fc59d6efcfa
lbarosi <lbarosi@df.ufcg.edu.br>
2022-10-25 13:03:56 -0300

    gnuradio ready.

--- radiotelescope/observations/observations.py
+++ radiotelescope/observations/observations.py
@@ -1,41 +1,35 @@
 # -*- coding: utf-8 -*-
 """
-Este módulo contém a classe OBSERVATIONS
+Este módulo contém a classe OBSERVATIONS.
 
 PACKAGE: Radiotelecope
 AUTHOR: Luciano Barosi
 DATE: 20.10.2022
 """
 # General Imports
-from datetime import datetime
-import itertools
-import sys
+import logging
 # Third Party Imports
-import astropy.coordinates as coord
 import astropy.units as u
-from astropy.time import Time, TimeDelta
 from matplotlib.colorbar import Colorbar
-import matplotlib.cm as cm
+from matplotlib import cm
 import matplotlib.dates as mdates
-import matplotlib.colors as colors
-from matplotlib.patches import Circle
 import matplotlib.pyplot as plt
-from matplotlib.ticker import (MultipleLocator, FormatStrFormatter,
-                               AutoMinorLocator)
 import numpy as np
 import pandas as pd
 import scipy as sp
-from scipy.signal import find_peaks
-from pytz import timezone
 # Local imports
-import radiotelescope.backend.backend as backend
-from radiotelescope.backend.backend import Backend as Backend
+# import radiotelescope.backend.backend as backend
+# from radiotelescope.backend.backend import Backend as Backend
+# Preparando log -----
+logger = logging.getLogger(__name__)
 # --------------------
 # main class definition.
 # --------------------
+
+
 class Observations:
-    """Short summary.
-    """
+    """Class Observations manipulate data already saved."""
+
     def __init__(self, t_start=None, duration=None, backend=None, path=None):
         """Instantiate and go."""
         self._t_start = t_start
@@ -44,23 +38,18 @@ class Observations:
         self._backend = backend
         self._path = path
         self.data = None
-        self.data_cal = None
-        self.timezone = None
         return
-
 
     @property
     def t_start(self):
         """Return the duration of observation."""
         return self._t_start
 
-
     @t_start.setter
     def t_start(self, t_start):
         """Set the initial time of observations."""
         self._t_start = t_start
         return
-
 
     @property
     def duration(self):
@@ -73,12 +62,10 @@ class Observations:
         self._duration = duration
         return
 
-
     @property
     def backend(self):
         """Return the object set as backend."""
         return self._backend
-
 
     @backend.setter
     def backend(self, backend):
@@ -86,12 +73,10 @@ class Observations:
         self._backend = backend
         return
 
-
     @property
     def path(self):
         """Return the duration of observation."""
         return self._path
-
 
     @path.setter
     def path(self, path):
@@ -100,7 +85,14 @@ class Observations:
         return
 
     def initialize(self):
+        """Inicia início e duração se não fornecido em `__init__`.
 
+        Fixa `timezone="UTC"`, início para 00:00h do dia atual e duração de 12 horas.
+
+        Returns:
+            type: object Observations.
+
+        """
         if self.backend is None:
             self.timezone = "UTC"
         else:
@@ -112,9 +104,21 @@ class Observations:
         self.t_end = self.t_start + self._duration
         return self
 
+    def filter_data(self, df, begin=None, freqs=None, duration=None,
+                    sampling=None):
+        """Filtra o dataframe com os parâmetros chamados.
 
-    def filter_data(self, df, begin = None, freqs = None, duration = None, sampling = None):
-        """Filter data from bachend in frequency, duration or sampling."""
+        Args:
+            df (pd.DataFrame): Se não fornecido utiliza `self.data`.
+            begin (pd.DateTime): tempo inicial `begin`. Se não fornecido é o início dos dados.
+            freqs (list): `[freqs_min, freqs_max]`.
+            duration (Quantity): Description of parameter `duration`. Defaults to None.
+            sampling (type): Ainda não implementado inteiramente.
+
+        Returns:
+            pd.DataFrame: dados filtrados.
+
+        """
         if begin is None:
             begin = df.index[0]
         else:
@@ -127,102 +131,116 @@ class Observations:
             freqmin = df.columns.min()
             freqmax = df.columns.max()
         if duration is not None:
-            end = begin + pd.Timedelta(seconds = duration.to(u.s).value)
-            #check if it fits inside original interval
+            end = begin + pd.Timedelta(seconds=duration.to(u.s).value)
+            # check if it fits inside original interval
             if end > df.index[-1]:
                 end = df.index[-1]
-        mask = df.columns.where((df.columns >= freqmin) & (df.columns < freqmax)).dropna()
+        mask = df.columns.where((df.columns >= freqmin) &
+                                (df.columns < freqmax)).dropna()
         df = df.loc[begin:end][mask]
         if sampling is not None:
             df = df.resample(sampling).mean()
         return df
 
-
-    def load_observation(self, mode="59", extension="fit"):  # SKY mode default.
-        """Check existent datafiles and retrieve information for observation as given parameters."""
+    def load_observation(self, mode="59", extension="fit"):
+        """Load observations from local files."""
         # Read filenames and parse timestamps
-        filenames = self.backend._get_filenames(extension=extension, mode=mode).filenames
+        filenames = self.backend._get_filenames(extension=extension,
+                                                mode=mode).filenames
         filenames = filenames.loc[self.t_start:self.t_end]
         try:
-            df = self.backend.load_measurement(filenames=filenames, mode=mode, extension=extension)
+            df = self.backend.load_measurement(filenames=filenames, mode=mode,
+                                               extension=extension)
             self.data = df
-        except ValueError as err:
+        except ValueError:
             print("No data found.")
             return None
         return self
 
-
     def calibrate(self, data=None, T_rx=None, TEMP=300, T_HOT=300, T_WARM=50):
         if T_rx is None:
             # carrega T_hot
-            df_hot = self.backend._get_filenames(extension="fit", mode="03").filenames
+            df_hot = self.backend._get_filenames(extension="fit",
+                                                 mode="03").filenames
             df_hot_data = self.backend.load_measurement(filenames=df_hot)
             hot_data = df_hot_data.median(axis=0)
-            V_hot = sp.signal.savgol_filter(hot_data, int(hot_data.shape[0]/50), 2, mode="nearest")
+            V_hot = sp.signal.savgol_filter(hot_data,
+                                            int(hot_data.shape[0]/50),
+                                            2,
+                                            mode="nearest")
             # carrega T_warm
-            df_warm = self.backend._get_filenames(extension="fit", mode="02").filenames
+            df_warm = self.backend._get_filenames(extension="fit",
+                                                  mode="02").filenames
             df_warm_data = self.backend.load_measurement(filenames=df_warm)
             warm_data = df_warm_data.median(axis=0)
-            V_warm = sp.signal.savgol_filter(warm_data, int(warm_data.shape[0]/50), 2, mode="nearest")
+            V_warm = sp.signal.savgol_filter(warm_data,
+                                             int(warm_data.shape[0]/50),
+                                             2,
+                                             mode="nearest")
             # temperatura do receptor
             Yc = 10**((V_hot-V_warm)/10)
             T_rx = (T_HOT - Yc * T_WARM)/(Yc-1)
         # carrega T_cold
-        df_cold = self.backend._get_filenames(extension="fit", mode="01").filenames
+        df_cold = self.backend._get_filenames(extension="fit",
+                                              mode="01").filenames
         df_cold_data = self.backend.load_measurement(filenames=df_cold)
         cold_data = df_cold_data.median(axis=0)
-        V_cold = sp.signal.savgol_filter(cold_data, int(cold_data.shape[0]/50), 2, mode="nearest")
+        V_cold = sp.signal.savgol_filter(cold_data,
+                                         int(cold_data.shape[0]/50),
+                                         2,
+                                         mode="nearest")
         # Calibrando
         Ys = 10**((data - V_cold)/10)
         T_A = T_rx * (Ys - 1) + Ys * TEMP
         # Verifica se calibração é muito antiga
         return T_A, T_rx
 
-
-    def plot_waterfall(self, df = None, freqs = None, duration = None, sampling = None, colorbar = True):
+    def plot_waterfall(self, df=None, freqs=None, duration=None, sampling=None,
+                       colorbar=True):
         freqs = freqs
         duration = duration
         sampling = sampling
-        if (freqs is not None) or (duration is not None) or (sampling is not None):
+        if ((freqs is not None) or (duration is not None) or
+                (sampling is not None)):
             # You may filter data inplace.
-            df = self.filter_data(df, freqs = freqs, sampling = sampling, duration = duration)
+            df = self.filter_data(df, freqs=freqs, sampling=sampling,
+                                  duration=duration)
         # limits
         freqs = df.columns
         begin = df.index[0]
         end = df.index[-1]
         mt = mdates.date2num((begin, end))
         hfmt = mdates.DateFormatter('%Y-%m-%d\n%H:%M:%S')
-        fmt_major = mdates.MinuteLocator(interval = 30)
-        fmt_minor = mdates.MinuteLocator(interval = 15)
-        #----------------
+        fmt_minor = mdates.MinuteLocator(interval=15)
         # average spectrum.
         SN = df.mean(axis=1)/df.std(axis=1)
-        spectrum = df.median(axis = 0)
-        #----------------
+        spectrum = df.median(axis=0)
         # create grid format.
         fig = plt.figure(figsize=(16, 10))
         grid = plt.GridSpec(10, 9, hspace=0.0, wspace=0.1)
         spectrum_ax = fig.add_subplot(grid[2:-2, :-1])
-        hor_fig = fig.add_subplot(grid[0:2, :-1], xticklabels=[], sharex=spectrum_ax)
+        hor_fig = fig.add_subplot(grid[0:2, :-1], xticklabels=[],
+                                  sharex=spectrum_ax)
         ver_fig = fig.add_subplot(grid[2:-2, -1], sharey=spectrum_ax)
-        #--------------------------
-        main = spectrum_ax.imshow(df.T, aspect='auto', extent = [ mt[0], mt[-1], freqs[-1], freqs[0]], cmap = cm.inferno)
+        main = spectrum_ax.imshow(df.T, aspect='auto',
+                                  extent=[mt[0], mt[-1], freqs[-1], freqs[0]],
+                                  cmap=cm.inferno)
         spectrum_ax.set_ylabel("Frequencies (MHz)")
         spectrum_ax.set_xlabel("Time")
         spectrum_ax.xaxis.set_major_formatter(hfmt)
         if colorbar:
-            cbax = fig.add_subplot(grid[9,:-1])
-            cb = Colorbar(ax = cbax, mappable = main, orientation = "horizontal", ticklocation = "bottom")
-        #--------------------------
+            cbax = fig.add_subplot(grid[9, :-1])
+            Colorbar(ax=cbax, mappable=main, orientation="horizontal",
+                     ticklocation="bottom")
+        # --------------------------
         # plot averaged spectrum in the vertical.
-        # plot averaged spectrum in the vertical.
-        ver_fig.plot(spectrum, freqs, c = 'red')
+        ver_fig.plot(spectrum, freqs, c='red')
         ver_fig.grid()
         ver_fig.yaxis.tick_right()
         ver_fig.yaxis.set_label_position('right')
         ver_fig.set_xlabel("Flux")
         # plot averaged spectrum in the vertical.
-        hor_fig.plot(df.index, SN, c = 'red')
+        hor_fig.plot(df.index, SN, c='red')
         hor_fig.set_ylabel("S/N")
         hor_fig.xaxis.tick_top()
         hor_fig.xaxis.set_minor_locator(fmt_minor)
@@ -230,45 +248,46 @@ class Observations:
         hor_fig.grid()
         return fig
 
-    def plot_ts(self, df = None, begin = None, duration = None, freqs = None):
+    def plot_ts(self, df=None, begin=None, duration=None, freqs=None):
         if begin is None:
             begin = self.t_start
         if df is None:
             df = self.df_data
-        df_filter = self.filter_data(df.loc[begin:], duration = duration, freqs = freqs)
+        df_filter = self.filter_data(df.loc[begin:], duration=duration,
+                                     freqs=freqs)
         fig = self.plot_timeseries(df_filter, self.df_sky)
         return fig
 
-#--------------------------------------
+
+# --------------------------------------
 # Utilidades para gráficos
-#--------------------------------------
-def _plot_waterfall(data = None, ax = None):
+# --------------------------------------
+def _plot_waterfall(data=None, ax=None):
     freqs = data.columns
     begin = data.index[0]
     end = data.index[-1]
     mt = mdates.date2num((begin, end))
     hfmt = mdates.DateFormatter('%Y-%m-%d\n%H:%M:%S')
-    fmt_major = mdates.MinuteLocator(interval = 30)
-    fmt_minor = mdates.MinuteLocator(interval = 15)
-    #--------------------------
+    # --------------------------
     if ax is None:
-        fig, ax = plt.subplots(figsize = (18,6))
-    main = ax.imshow(data.T, aspect='auto', extent = [ mt[0], mt[-1], freqs[-1], freqs[0]], cmap = cm.inferno)
+        fig, ax = plt.subplots(figsize=(18, 6))
+    ax.imshow(data.T, aspect='auto', extent=[mt[0], mt[-1], freqs[-1],
+              freqs[0]], cmap=cm.inferno)
     ax.set_ylabel("Frequencies (MHz)")
     ax.set_xlabel("Time")
     ax.xaxis.set_major_formatter(hfmt)
     return ax
 
 
-def plot_mosaic(data = None, ax = None):
-    axes = [];
+def plot_mosaic(data=None, ax=None):
+    axes = []
     num = len(data)
     if (num == 3) or (num > 4):
-            cols = 3
+        cols = 3
     else:
         cols = 2
     rows = int(np.ceil(num / cols))
-    dfs = np.empty([rows, cols], dtype = "object")
+    dfs = np.empty([rows, cols], dtype="object")
     ii = 0
     if rows > 1:
         for row in np.arange(rows):
@@ -280,14 +299,14 @@ def plot_mosaic(data = None, ax = None):
             dfs[0, col] = data[ii]
             ii += 1
     if ax is None:
-        fig, ax = plt.subplots(nrows = rows, ncols = cols, figsize=(18,6))
+        fig, ax = plt.subplots(nrows=rows, ncols=cols, figsize=(18, 6))
     if rows > 1:
         for row in np.arange(rows):
             for col in np.arange(cols):
-                axes.append(_plot_waterfall(data = dfs[row, col], ax = ax[row, col]));
+                axes.append(_plot_waterfall(data=dfs[row, col],
+                                            ax=ax[row, col]))
     else:
         for col in np.arange(cols):
-            axes.append(_plot_waterfall(data = dfs[0,col], ax = ax[col]));
+            axes.append(_plot_waterfall(data=dfs[0, col],
+                                        ax=ax[col]))
     return axes
-
-#--------------------------------------
